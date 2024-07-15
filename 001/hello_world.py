38440068527c99c32815ad0598c7ef580c7e1019
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-07-15 18:21:09 -0300

    Add comentário hello_world.py

--- 001/hello_world.py
+++ 001/hello_world.py
@@ -8,8 +8,14 @@ if 5 > 2:
 print('próxima ação')
 
 
-if 5 > 2:
+if 5 > 2: # aqui entra um comentário
     print("Five is greater than two!")
     print("Five is greater than two - another!")
 if 5 > 2:
         print("Five is greater than two!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
