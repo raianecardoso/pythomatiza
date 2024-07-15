ca0bbb4b503404e942a907f14a77c152b113628b
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-07-15 17:53:57 -0300

    Add primeiros exemplos - identação

---
+++ 001/hello_world.py
@@ -0,0 +1,15 @@
+print('Hello world')
+print('Gabriel')
+print(2+2)
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+print('próxima ação')
+
+
+if 5 > 2:
+    print("Five is greater than two!")
+    print("Five is greater than two - another!")
+if 5 > 2:
+        print("Five is greater than two!")
