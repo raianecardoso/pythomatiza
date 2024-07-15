3f2d20aae9c2b0bcfd266c1c0b2592e563ceb955
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-07-15 18:21:44 -0300

    Add exemplo de utilização função

---
+++ 001/function.py
@@ -0,0 +1,11 @@
+
+def soma(a, b):
+    """
+    Esta é uma função de soma de dois números
+    Estou comentando em mais de uma linha
+    """
+    print('Estou somando a + b. O resultado é:')
+    print(a+b)
+
+soma(1, 1)
+soma(1, 3)
