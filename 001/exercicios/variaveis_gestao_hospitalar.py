d5f46ae678aed2c19a99d5ddb486d99125b7549c
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-07-22 16:00:42 -0300

    Add primeira versao exercicio hospital - aula 1

---
+++ 001/exercicios/variaveis_gestao_hospitalar.py
@@ -0,0 +1,13 @@
+# Um novo paciente chamado João deu entrada no hospital.
+# Ele tem 20 anos e é um novo paciente deste hospital.
+# Defina variáveis para armazenar o nome deste paciente,
+# sua idade e se ele é um novo paciente da instituição.
+# Mostre no sistema todas estas informações coletadas.
+
+# nome
+# paciente
+# name
+nome_paciente = 'João' # str
+idade = 20 #int
+novo_paciente = True
+print(nome_paciente, idade, novo_paciente)
