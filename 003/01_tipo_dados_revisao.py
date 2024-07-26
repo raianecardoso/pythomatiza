a81c5f73b0a1ac318f9bdf0d9cfb74b3bd3cb078
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-07-26 16:22:15 -0300

    Add primeiro exemplo encontro 3 variaveis

---
+++ 003/01_tipo_dados_revisao.py
@@ -0,0 +1,10 @@
+# Revisão dos tipos de dados
+nome = 'Gabriel' #str
+# Números
+idade = 18 #int
+valor = 3.5 #float
+pi = 3.14198 #float
+# Booleanos
+novo_paciente = True # True e False sempre com a primeira letra maiúscula
+novo_medico = False
+
