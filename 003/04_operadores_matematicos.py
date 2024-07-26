acd5ba9b00770f268c658a88a598be098b29210c
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-07-26 17:37:20 -0300

    Add exemplo operadores matemáticos
    Definição para fazer exercicios operadores e áreas

---
+++ 003/04_operadores_matematicos.py
@@ -0,0 +1,8 @@
+print(1+1) #soma
+print(8-4) #subtração
+print(2*2) #multiplicação
+print(4/2) #divisão com casa decimal
+print(5/2) #divisão com casa decimal
+print(5//2) #divisão inteira
+print(2**2) #expo
+print(5%2) #resto da divisão
