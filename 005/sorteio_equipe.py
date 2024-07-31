195e53bf333b270ebf73616ada1f69f9139ec19c
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-07-31 17:46:04 -0300

    Melhora solução para sorteio equipe
    random.choice
    See #15

--- 005/sorteio_equipe.py
+++ 005/sorteio_equipe.py
@@ -1,16 +1,16 @@
 import random
 
 def sorteio():
-    equipe = {
-        1: 'André',
-        2: 'Cora',
-        3: 'Felipe',
-        4: 'Gabriel',
-        5: 'Henrique',
-        6: 'Isabelle',
-        7: 'Raiane',
-        8: 'Romano',
-        9: 'Yan',
-    }
-    sorteado = random.randrange(1, 10)
-    return f'Foi sorteado o número {sorteado} e, portanto, o escolhido para organizar nosso próximo hh é {equipe[sorteado]}'
+    equipe = [
+        'André',
+        'Cora',
+        'Felipe',
+        'Gabriel',
+        'Henrique',
+        'Isabelle',
+        'Raiane',
+        'Romano',
+        'Yan',
+    ]
+    sorteado = random.choice(equipe)
+    return f'O escolhido para organizar nosso próximo hh é {sorteado}'
