a7988136e9407239e45496572a22455a3fe51fb0
Raiane Cardoso <raianecardoso.rede@gmail.com>
2024-08-01 15:38:06 -0300

    Resolve o exercício 03 operadore

--- web_dev_challenges/01_essencial/03_operadores/operators.py
+++ web_dev_challenges/01_essencial/03_operadores/operators.py
@@ -1,28 +1,29 @@
 def add(first_number, second_number):
     # TODO: retorne a soma dos parâmetros first_number e second_number
-    pass
+    return first_number + second_number
 
 def sub(first_number, second_number):
     # TODO: retorne a subtração dos parâmetros first_number e second_number
-    pass
+    return first_number - second_number
 
 def mult(first_number, second_number):
     # TODO: retorne a multiplicação dos parâmetros first_number e second_number
-    pass
+    return first_number * second_number
 
 def div(first_number, second_number):
     # TODO: retorne a divisão dos parâmetros first_number e second_number
-    pass
+    return first_number / second_number
 
 def expo(first_number, second_number):
     # TODO: retorne a exponenciação dos parâmetros first_number e second_number
-    pass
+    return first_number ** second_number
 
 def remai(first_number, second_number):
     # TODO: retorne o resto da divisão dos parâmetros first_number e second_number
-    pass
+    return first_number % second_number
 
 def quoti_remai(first_number, second_number):
     # TODO: retorne o quociente e o resto da divisão, nesta exata ordem, dos parâmetros first_number e second_number
     # Hint: Utilize a vírgula para separar os dois valores retornados na função
-    pass
+    quociente = first_number // second_number
+    return quociente, remai(first_number, second_number)
