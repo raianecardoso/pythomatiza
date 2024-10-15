8ca6c834e015becae4dca6fe5eee4b3a711cc7e3
Raiane Cardoso <raianecardoso.rede@gmail.com>
2024-10-15 16:27:02 -0300

    Inclui ex list e tuple

--- web_dev_challenges/02_lists_tuple_dict/01_lista_diversos/list_methods.py
+++ web_dev_challenges/02_lists_tuple_dict/01_lista_diversos/list_methods.py
@@ -1,33 +1,36 @@
 def sort_asc(my_list):
     # TODO: Retorne a lista de nomes recebida em ordem alfabética ascendente
-    pass
+    my_list.sort()
+    return my_list
 
 def sort_desc(my_list):
     # TODO: Retorne a lista de nomes recebida em ordem alfabética descendente
-    pass
+    my_list.sort(reverse = True)
+    return my_list
 
 def find_list_element(my_list):
     # TODO: Retorne o segundo elemento da lista recebida
-    pass
+    return my_list[1]
 
 def find_last_list_element(my_list):
     # TODO: Retorne o último elemento da lista recebida
-    pass
+    return my_list[-1]
 
 def find_out_of_range_error():
     # TODO: Encontrar o erro no código abaixo
     # TODO: my_list não deverá ser modificada
     my_list = [1, 2, 3, 4, 5]
-    return my_list[5]
+    return my_list[4]
 
 def x_not_in_the_list_error():
     # TODO: Encontrar o erro no código abaixo
     # TODO: my_list não deverá ser modificada
     # TODO: Último elemento da lista deverá ser removido
     my_list = [1, 2, 3, 4, 5]
-    my_list.remove(6)
+    my_list.remove(5)
     return my_list[0]
 
 def list_remove_last(my_list):
     # TODO: Retorne a lista recebida como argumento sem o último elemento
-    pass
+    my_list.pop() 
+    return my_list
--- web_dev_challenges/02_lists_tuple_dict/03_tuple_diversos/tuple_methods.py
+++ web_dev_challenges/02_lists_tuple_dict/03_tuple_diversos/tuple_methods.py
@@ -1,16 +1,21 @@
 def tuple_sum():
     # TODO: Calcule a soma dos elementos da  tupla 'my_tuple'
     my_tuple = 1, 2, 3, 4, 5
-    pass
+    soma = 0
+    for i in range(len(my_tuple)):
+        soma += my_tuple[i]
+    return soma
 
 def find_second_element():
     # TODO: Retorne o segundo elemento da tupla 'my_tuple'
     my_tuple = 1, 2, 3, 4, 5
-    pass
+    return my_tuple[1]
 
 def find_out_typle_error():
     # TODO: Encontrar o erro no código abaixo
     # TODO: my_tuple não deverá ser modificada
     my_tuple = 1, 2, 3, 4, 5
-    typle.remove(1)
+    # i = list(my_tuple)
+    # i.remove(1)
+    # my_tuple = tuple(i)
     return my_tuple
